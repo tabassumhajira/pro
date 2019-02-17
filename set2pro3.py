343d4ad80567812ac90aeb940f2bd0e7e1040a1e
tabassumhajira <42860316+tabassumhajira@users.noreply.github.com>
2019-02-17 20:35:33 +0530

    Create set2pro3.py

---
+++ set2pro3.py
@@ -0,0 +1,8 @@
+import sys, string, math
+r,s = input().split()
+r,s = int(n), int(k)
+L = [ int(x) for x in input().split()]
+for i in range(0,s) :
+    m,n = input().split()
+    m,n = int(m), int(n)
+    print(min(L[m-1:n]))
